855a2cbf2da16ffef09347435e639b6af9192bd3
Victor Lopez <victorlopezlopez93@gmail.com>
2026-02-17 12:12:04 +0000

    chore: initial project setup with Spring Boot 3 and Docker Compose

---
+++ src/test/java/com/victorlopez/incident_api/IncidentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorlopez.incident_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
