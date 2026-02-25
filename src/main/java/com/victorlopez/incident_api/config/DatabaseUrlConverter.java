2cb5802821396e86b0c93a19067c586400ffb76f
Victor Lopez <victorlopezlopez93@gmail.com>
2026-02-25 18:40:48 +0100

    fix: disable autoCommit in custom Hikari DataSource configuration

--- src/main/java/com/victorlopez/incident_api/config/DatabaseUrlConverter.java
+++ src/main/java/com/victorlopez/incident_api/config/DatabaseUrlConverter.java
@@ -33,19 +33,24 @@ public class DatabaseUrlConverter {
                     .type(HikariDataSource.class)
                     .build();
             dataSource.setJdbcUrl(jdbcUrl);
-            
+            dataSource.setAutoCommit(false);
+
             // Set connection pool properties
             dataSource.setMaximumPoolSize(Integer.parseInt(environment.getProperty("DB_POOL_SIZE", "10")));
             dataSource.setMinimumIdle(Integer.parseInt(environment.getProperty("DB_POOL_MIN_IDLE", "2")));
             dataSource.setConnectionTimeout(Long.parseLong(environment.getProperty("DB_CONNECTION_TIMEOUT", "30000")));
             dataSource.setIdleTimeout(Long.parseLong(environment.getProperty("DB_IDLE_TIMEOUT", "600000")));
             dataSource.setMaxLifetime(Long.parseLong(environment.getProperty("DB_MAX_LIFETIME", "1800000")));
-            
+
             log.info("Successfully configured DataSource with converted JDBC URL");
             return dataSource;
         } else {
             log.info("DATABASE_URL not in postgresql:// format, using default configuration");
-            return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+            HikariDataSource dataSource = properties.initializeDataSourceBuilder()
+                    .type(HikariDataSource.class)
+                    .build();
+            dataSource.setAutoCommit(false);
+            return dataSource;
         }
     }
 }
